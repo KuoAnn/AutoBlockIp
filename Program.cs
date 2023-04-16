56a625d75eb909e9858d866555026c7286eea2e1
KuoAnn <b54744040@gmail.com>
2023-04-16 14:48:01 +0800

    [Layout] Update

--- Program.cs
+++ Program.cs
@@ -2,234 +2,228 @@
 using System.Management.Automation;
 using System.Reflection;
 using System.Text;
+using Serilog.Events;
+using Serilog;
 
 namespace AutoBlockIP
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:驗證平台相容性", Justification = "<暫止>")]
-    internal class Program
-    {
-        private static readonly int threshold = 3;
-        private static readonly string[] whiteList = new string[] { "kuoann" };
-        private static readonly string[] blackList = new string[] { "administrator", "guest" };
-        private static readonly string firewallRuleName = "AutoBlockIP";
-        private static StringBuilder logMessage = new StringBuilder();
+	internal class Program
+	{
+		private static readonly int threshold = 3;
+		private static readonly string[] whiteList = new string[] { "kuoann" };
+		private static readonly string[] blackList = new string[] { "administrator", "guest" };
+		private static readonly string firewallRuleName = "AutoBlockIP";
+		private static StringBuilder logMessage = new StringBuilder();
 
-        private static void Main(string[] args)
-        {
-            try
-            {
-                var ips = GetSuspiciousIps();
+		private static void Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug,
+					outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}]{Message}{NewLine}{Exception}")
+				.WriteTo.Seq("http://localhost:1315",
+					restrictedToMinimumLevel: LogEventLevel.Information,
+					bufferBaseFilename: @"C:\Logs\Seq")
+				.CreateLogger();
 
-                if (ips.Count() > 0)
-                {
-                    var blockedIps = GetBlockedIps();
-                    var mergedIps = blockedIps.Union(ips);
+			try
+			{
+				var ips = GetSuspiciousIps();
+				if (ips.Count() > 0)
+				{
+					var blockedIps = GetBlockedIps();
+					var unionBlockIps = blockedIps.Union(ips);
 
-                    var newBlockIps = mergedIps.Except(blockedIps);
-                    mergedIps = mergedIps.OrderBy(x => x).ToList();
+					var newBlockIps = unionBlockIps.Except(blockedIps);
+					unionBlockIps = unionBlockIps.OrderBy(x => x).ToList();
 
-                    if (newBlockIps.Count() > 0)
-                    {
-                        Log($" >>> Add [{string.Join("\n", newBlockIps)}]");
-                        if (SetBlockedIpsIntoFirewall(mergedIps.ToArray()))
-                        {
-                            Log("SetBlockedIpsIntoFirewall...OK");
-                        }
-                        else
-                        {
-                            Log("SetBlockedIpsIntoFirewall...Fail");
-                        }
-                    }
-                    else
-                    {
-                        Log($"\nNo Updated IP...{ips.Count() / blockedIps.Count()}");
-                    }
-                }
-                else
-                {
-                    Log($"\nNo Suspicious IP");
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Log("\nError >>> " + ex.ToString());
-                Write2EventLog(ex.ToString(), EventLogEntryType.Error);
-            }
-            finally
-            {
+					if (newBlockIps.Count() > 0)
+					{
+						AppendLog($" >> Add [{string.Join("\n", newBlockIps)}]");
+						if (SetFirewall(unionBlockIps.ToArray()))
+						{
+							Log.Information($"SetFirewall...OK ({unionBlockIps.Count()})");
+						}
+						else
+						{
+							Log.Error($"SetFirewall...Fail ({unionBlockIps.Count()})");
+						}
+					}
+				}
+				else
+				{
+					AppendLog($"\nUnextract IP");
+				}
+			}
+			catch (Exception ex)
+			{
+				AppendLog("\nError >> " + ex.ToString());
+				Log.Fatal($"[AutoBlockIP] Excep: {ex.Message}", ex);
+			}
+			finally
+			{
+				if (!string.IsNullOrEmpty(logMessage.ToString().Trim()))
+				{
+					Log.Warning(logMessage.ToString());
+				}
+				else
+				{
+					Log.Debug("[AutoBlockIP] Done");
+				}
 #if DEBUG
-                Console.ReadKey();
-#else
-                Write2EventLog(logMessage.ToString(), EventLogEntryType.Warning);
+				Console.ReadKey();
 #endif
-            }
-        }
+			}
+		}
 
-        private static void Log(string msg)
-        {
-            logMessage.AppendLine(msg);
-            Console.WriteLine(msg);
-        }
+		private static void AppendLog(string msg)
+		{
+			logMessage.AppendLine(msg);
+		}
 
-        private static void Write2EventLog(string message, EventLogEntryType entryType = EventLogEntryType.Information)
-        {
-            var assemblyVersion = Assembly.GetEntryAssembly()?.GetName().Version;
+		/// <summary>
+		/// Get suspicious ips from event log
+		/// </summary>
+		private static List<string> GetSuspiciousIps()
+		{
+			var suspiciousIps = new List<string>();
+			var eventLog = new EventLog() { Log = "Security" };
+			var entries =
+				from EventLogEntry e in eventLog.Entries
+				where e.InstanceId == 4625
+					&& e.EntryType == EventLogEntryType.FailureAudit
+					&& e.TimeGenerated > DateTime.Now.AddMinutes(-10)
+				select new
+				{
+					e.ReplacementStrings,
+				};
 
-            using (var eventLog = new EventLog("Application"))
-            {
-                eventLog.Source = "WebApp";
-                eventLog.WriteEntry($"[{firewallRuleName}][v{assemblyVersion}] {message}", entryType);
-            }
-        }
+			if (entries.Count() > 0)
+			{
+				var events = entries.ToList();
+				var ips = new Dictionary<string, int>();
 
-        /// <summary>
-        /// Get suspicious ips from event log
-        /// </summary>
-        private static List<string> GetSuspiciousIps()
-        {
-            var suspiciousIps = new List<string>();
-            var eventLog = new EventLog() { Log = "Security" };
-            var entries =
-                from EventLogEntry e in eventLog.Entries
-                where e.InstanceId == 4625
-                    && e.EntryType == EventLogEntryType.FailureAudit
-                    && e.TimeGenerated > DateTime.Now.AddMinutes(-15)
-                select new
-                {
-                    e.ReplacementStrings,
-                };
+				foreach (var d in events)
+				{
+					if (d.ReplacementStrings.Length >= 19)
+					{
+						var targetUserName = d.ReplacementStrings[5];
+						var ip = d.ReplacementStrings[19];
 
-            if (entries.Count() > 0)
-            {
-                var events = entries.ToList();
-                var ips = new Dictionary<string, int>();
+						if (ValidateIPv4(ip) && (!IsWhiteList(targetUserName)))
+						{
+							if (ips.ContainsKey(ip))
+							{
+								ips[ip]++;
+							}
+							else
+							{
+								ips.Add(ip, 1);
+							}
+						}
+					}
+				}
 
-                foreach (var d in events)
-                {
-                    if (d.ReplacementStrings.Length >= 19)
-                    {
-                        var targetUserName = d.ReplacementStrings[5];
-                        var ip = d.ReplacementStrings[19];
+				suspiciousIps = ips.Where(x => IsBlackList(x.Key) || x.Value > threshold)
+					.Select(x => x.Key).ToList();
 
-                        if (ValidateIPv4(ip) && (!IsWhiteList(targetUserName)))
-                        {
-                            if (ips.ContainsKey(ip))
-                            {
-                                ips[ip]++;
-                            }
-                            else
-                            {
-                                ips.Add(ip, 1);
-                            }
-                        }
-                    }
-                }
+				AppendLog($"[{string.Join(",", suspiciousIps)}]");
+			}
+			else
+			{
+				AppendLog($"Event 4625 Not Found in \"{eventLog.LogDisplayName}\"");
+			}
 
-                suspiciousIps = ips.Where(x => IsBlackList(x.Key) || x.Value > threshold)
-                    .Select(x => x.Key).ToList();
+			return suspiciousIps;
+		}
 
-                Log($"[{string.Join(",", suspiciousIps)}]");
-            }
-            else
-            {
-                Log($"Event 4625 Not Found in \"{eventLog.LogDisplayName}\"");
-            }
+		private static bool ValidateIPv4(string ipString)
+		{
+			if (String.IsNullOrWhiteSpace(ipString))
+			{
+				return false;
+			}
 
-            return suspiciousIps;
-        }
+			string[] splitValues = ipString.Split('.');
+			if (splitValues.Length != 4)
+			{
+				return false;
+			}
 
-        private static bool ValidateIPv4(string ipString)
-        {
-            if (String.IsNullOrWhiteSpace(ipString))
-            {
-                return false;
-            }
+			byte tempForParsing;
 
-            string[] splitValues = ipString.Split('.');
-            if (splitValues.Length != 4)
-            {
-                return false;
-            }
+			return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+		}
 
-            byte tempForParsing;
+		private static bool IsWhiteList(string targetUserName) =>
+			!string.IsNullOrWhiteSpace(targetUserName)
+			&& whiteList.Contains(targetUserName.Trim().ToLower());
 
-            return splitValues.All(r => byte.TryParse(r, out tempForParsing));
-        }
+		private static bool IsBlackList(string targetUserName) =>
+			!string.IsNullOrWhiteSpace(targetUserName)
+			&& blackList.Contains(targetUserName.Trim().ToLower());
 
-        private static bool IsWhiteList(string targetUserName) =>
-            !string.IsNullOrWhiteSpace(targetUserName)
-            && whiteList.Contains(targetUserName.Trim().ToLower());
+		/// <summary>
+		/// Get Blocked ips from firewall
+		/// </summary>
+		private static List<string> GetBlockedIps()
+		{
+			var blockedIps = new List<string>();
 
-        private static bool IsBlackList(string targetUserName) =>
-            !string.IsNullOrWhiteSpace(targetUserName)
-            && blackList.Contains(targetUserName.Trim().ToLower());
+			using (var ps = PowerShell.Create())
+			{
+				ps.AddScript("Set-ExecutionPolicy RemoteSigned");
+				ps.AddScript("Import-Module NetSecurity");
+				ps.AddScript($"[string[]](Get-NetFirewallRule -DisplayName '{firewallRuleName}' | Get-NetFirewallAddressFilter).RemoteAddress");
 
-        /// <summary>
-        /// Get Blocked ips from firewall
-        /// </summary>
-        private static List<string> GetBlockedIps()
-        {
-            var blockedIps = new List<string>();
+				foreach (string ip in ps.Invoke<string>())
+				{
+					blockedIps.Add(ip);
+				}
 
-            using (var ps = PowerShell.Create())
-            {
-                ps.AddScript("Set-ExecutionPolicy RemoteSigned");
-                ps.AddScript("Import-Module NetSecurity");
-                ps.AddScript($"[string[]](Get-NetFirewallRule -DisplayName '{firewallRuleName}' | Get-NetFirewallAddressFilter).RemoteAddress");
+				PSDataCollection<ErrorRecord> errors = ps.Streams.Error;
+				if (errors != null && errors.Count > 0)
+				{
+					foreach (ErrorRecord err in errors)
+					{
+						Log.Error($"[AutoBlockIP] Error: {err}");
+					}
+				}
+			}
 
-                foreach (string ip in ps.Invoke<string>())
-                {
-                    blockedIps.Add(ip);
-                }
+			return blockedIps;
+		}
 
-                PSDataCollection<ErrorRecord> errors = ps.Streams.Error;
-                if (errors != null && errors.Count > 0)
-                {
-                    foreach (ErrorRecord err in errors)
-                    {
-                        Write2EventLog($"Error: {err}", EventLogEntryType.Error);
-                    }
-                }
-            }
+		/// <summary>
+		/// Set Blocked ips into firewall
+		/// </summary>
+		private static bool SetFirewall(string[] blockIps)
+		{
+			using (var ps = PowerShell.Create())
+			{
+				var sb = new StringBuilder();
+				sb.Append("\"");
+				sb.Append(string.Join("\",\"", blockIps));
+				sb.Append("\"");
 
-            return blockedIps;
-        }
+				ps.AddScript("Set-ExecutionPolicy RemoteSigned");
+				ps.AddScript("Import-Module NetSecurity");
+				ps.AddScript($"Set-NetFirewallRule -DisplayName '{firewallRuleName}' -Direction Inbound -Action Block -RemoteAddress @({sb})");
 
-        /// <summary>
-        /// Set Blocked ips into firewall
-        /// </summary>
-        private static bool SetBlockedIpsIntoFirewall(string[] ips)
-        {
-            using (var ps = PowerShell.Create())
-            {
-                var sb = new StringBuilder();
-                sb.Append("\"");
-                sb.Append(string.Join("\",\"", ips));
-                sb.Append("\"");
+				ps.Invoke();
 
-                ps.AddScript("Set-ExecutionPolicy RemoteSigned");
-                ps.AddScript("Import-Module NetSecurity");
-                ps.AddScript($"Set-NetFirewallRule -DisplayName '{firewallRuleName}' -Direction Inbound -Action Block -RemoteAddress @({sb})");
+				PSDataCollection<ErrorRecord> errors = ps.Streams.Error;
+				if (errors != null && errors.Count > 0)
+				{
+					foreach (ErrorRecord err in errors)
+					{
+						Log.Error($"[AutoBlockIP] Error: {err}");
+					}
 
-                ps.Invoke();
-
-                PSDataCollection<ErrorRecord> errors = ps.Streams.Error;
-                if (errors != null && errors.Count > 0)
-                {
-                    foreach (ErrorRecord err in errors)
-                    {
-                        Write2EventLog($"Error: {err}", EventLogEntryType.Error);
-                    }
-
-                    return false;
-                }
-                else
-                {
-                    Log($"{string.Join("\n", ips)}");
-                }
-            }
-            return true;
-        }
-    }
+					return false;
+				}
+			}
+			return true;
+		}
+	}
 }
